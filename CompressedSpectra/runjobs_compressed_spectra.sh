2d36a237d53f488ddab4bad0cd234758c951ce8c
gk199 <gillian.kopp@gmail.com>
2020-10-27 22:04:27 +0100

    files for running compressed spectra gridpack to LHE

---
+++ CompressedSpectra/runjobs_compressed_spectra.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export X509_USER_PROXY=$1
+voms-proxy-info -all
+voms-proxy-info -all -file $1
+
+condor_argu=$2
+
+cd /afs/cern.ch/user/g/gkopp/nobackup/cmsdas_2020_gen/genprod_mg261_slc7/bin/MadGraph5_aMCatNLO
+eval `scramv1 runtime -sh`
+mkdir $condor_argu
+cd $condor_argu
+tar xf ../nfwLLP2002203m_slc7_amd64_gcc700_CMSSW_10_6_0_tarball.tar.xz
+NEVENTS=10000
+RANDOMSEED=12345
+NCPU=1
+./runcmsgrid.sh $NEVENTS $RANDOMSEED $NCPU
+
